f5957e8673c05bcd608731f26707376fdd89d8c3
zei-games <151435816+zei-games@users.noreply.github.com>
2023-11-19 19:27:50 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let nav = document.querySelector("#navArea")
+let btn = document.querySelector(".toggle-btn")
+let mask = document.querySelector("#mask")
+
+btn.onclick = () => {
+	nav.classList.toggle("open");
+};
